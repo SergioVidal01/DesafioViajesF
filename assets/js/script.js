851f71fba02a228801d7ca69e383c4bb1873b10e
Sergio Vidal <s.vidal.bravo.92@gmail.com>
2024-08-13 21:37:29 -0400

    ultimos cambios e integracion del modal

--- assets/js/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('termsLink').addEventListener('click', function(event) {
-    event.preventDefault(); // Previene el desplazamiento hacia arriba
-    var termsModal = new bootstrap.Modal(document.getElementById('termsModal'));
-    termsModal.show();
-});
